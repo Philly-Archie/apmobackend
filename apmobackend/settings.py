4f6b0561592a68d319ef061d853c0b0021daee19
Philly Olowo <olowophilly11@gmail.com>
2024-12-18 12:58:53 +0300

    db config changes

--- apmobackend/settings.py
+++ apmobackend/settings.py
@@ -91,17 +91,17 @@ WSGI_APPLICATION = 'apmobackend.wsgi.application'
 # }
 
 
-if os.getenv('RENDER'):
-    DATABASES = {
-        'default': dj_database_url.parse(config("DATABASE_URL"))
+# if os.getenv('RENDER'):
+#     DATABASES = {
+#         'default': dj_database_url.parse(config("DATABASE_URL"))
+#     }
+# else:
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
